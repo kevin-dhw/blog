7f74d931475cb8e33cf55e70c6e151b84bf8900b
kevin <932731452@qq.com>
2025-07-10 11:57:49 +0800

    init

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,21 +1,66 @@
 import React, { useState } from "react";
-import Title from "./components/title";
-import Tab from "./components/tab";
+import classNames from "classnames";
+import avatarImg from "./img/avatar.jpg";
 
 const Home: React.FC = () => {
-  const [idx, setIdx] = useState(0);
-  const getCurItemIdx = (idx: number) => {
-    setIdx(idx);
-  };
+  const [tabs, setTabs] = useState([
+    {
+      isSelected: true,
+      label: "Blog",
+    },
+    {
+      isSelected: false,
+      label: "Projects",
+    },
+    {
+      isSelected: false,
+      label: "Learn",
+    },
+    {
+      isSelected: false,
+      label: "Me",
+    },
+  ]);
+
   return (
-    <div className=" flex flex-col">
-      <Title></Title>
-      <Tab getCurItemIdx={getCurItemIdx}></Tab>
-      <div className=" p-[20px] px-[200px]">
-        {idx === 0 && <div>000000</div>}
-        {idx === 1 && <div>111111</div>}
-        {idx === 2 && <div>222222</div>}
-        {idx === 3 && <div>333333</div>}
+    <div className=" flex justify-between p-[20px] items-center">
+      <div className=" flex">
+        {tabs.map((item, index) => {
+          return (
+            <div
+              onClick={() => {
+                console.log(12344, index);
+                setTabs((data) => {
+                  data.forEach((item, idx) => {
+                    if (idx === index) {
+                      item.isSelected = true;
+                    } else {
+                      item.isSelected = false;
+                    }
+                  });
+                  return [...data];
+                });
+              }}
+              key={index}
+              className={classNames(
+                " mr-[14px] text-gray-400 cursor-pointer text-[18px]",
+                " hover:text-gray-600 duration-300 ease-in-out delay-100",
+                item.isSelected && " text-black"
+              )}
+            >
+              {item.label}
+            </div>
+          );
+        })}
+      </div>
+      <div>
+        <div className=" flex items-center">
+          <span>Kevin Du </span>
+          <img
+            className=" w-[40px] h-[40px] rounded-full ml-[20px]"
+            src={avatarImg}
+          ></img>
+        </div>
       </div>
     </div>
   );
